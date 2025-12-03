55eef4f4f1a2437e474baca93046b3d53b053dbf
salmane <salmajo2001@gmail.com>
2025-12-03 18:05:04 +0100

    i think it works lol?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+        Scanner scanner = new Scanner(System.in);
         System.out.print("$ ");
+        String command = scanner.nextLine();
+        StringBuilder builder = new StringBuilder(command);
+        builder.append(": command not found ");
+        System.out.println(builder.toString());
+
     }
 }
