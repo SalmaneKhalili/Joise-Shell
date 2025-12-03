748baa49a669e5aa97b3d72bd2a35d764b69bf68
salmane <salmajo2001@gmail.com>
2025-12-03 18:08:44 +0100

    repl done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
-        System.out.print("$ ");
-        String command = scanner.nextLine();
-        StringBuilder builder = new StringBuilder(command);
-        builder.append(": command not found ");
-        System.out.println(builder.toString());
+        while (true) {
+            System.out.print("$ ");
+            String command = scanner.nextLine();
+            StringBuilder builder = new StringBuilder(command);
+            builder.append(": command not found ");
+            System.out.println(builder.toString());
+        }
+
+
 
     }
 }
